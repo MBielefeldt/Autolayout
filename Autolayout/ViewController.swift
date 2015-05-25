bcfea01add57d72bcbfde0e2535e7db2df72a9db
Mads Bielefeldt <mads.bielefeldt@gmail.com>
2015-05-26 00:36:30 +0200

    Images and User.swift added

--- Autolayout/ViewController.swift
+++ Autolayout/ViewController.swift
@@ -10,6 +10,43 @@ import UIKit
 
 class ViewController: UIViewController
 {
-
+    @IBOutlet weak var usernameTextField: UITextField!
+    
+    @IBOutlet weak var passwordLabel: UILabel!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        updateUI()
+    }
+    
+    private func updateUI()
+    {
+        passwordTextField.secureTextEntry = secure
+        passwordLabel.text = secure ? "Secured Password" : "Password"
+    }
+    
+    var secure = false {
+        didSet {
+            updateUI()
+        }
+    }
+    
+    @IBAction func changeSecurityButton(sender: AnyObject)
+    {
+        secure = !secure
+    }
+    
+    var loggedInUser: User? {
+        didSet {
+            updateUI()
+        }
+    }
+    
+    @IBAction func loginButton(sender: AnyObject)
+    {
+        loggedInUser = User.login(usernameTextField.text ?? "", password: passwordTextField.text ?? "")
+    }
 }
 
